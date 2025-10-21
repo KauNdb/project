ff0b9c2aaf4f2775ba532d8da68ab5be1b513237
Mikhail <icemi.mikhail@mail.ru>
2025-10-21 21:13:58 +0200

    update cart

--- internal/auth/repository.go
+++ internal/auth/repository.go
@@ -30,14 +30,14 @@ func (authRepo *AuthRepository) UpatePhone(phone *Phone) (*Phone, error) {
 	return phone, nil
 }
 
-func (authRepo *AuthRepository) GetPhone(phoneNum string) (*Phone, error) {
+func (authRepo *AuthRepository) GetPhone(phoneNum string) (uint, error) {
 	var phone Phone
 	result := authRepo.DataBase.DB.First(&phone, "phone = ?", phoneNum)
 	if result.Error != nil {
-		return nil, result.Error
+		return 0, result.Error
 	}
 
-	return &phone, nil
+	return phone.ID, nil
 }
 
 func (authRepo *AuthRepository) GetPhoneByCode(sessionId string, code int) (*Phone, error) {
